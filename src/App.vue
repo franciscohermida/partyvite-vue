f3e5a2385ce2da31e8e96e4caba89042ca3bd31f
Francisco Hermida <483242+franciscohermida@users.noreply.github.com>
2025-02-15 10:25:11 +1100

    feat: embellish the xp

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
 import PartySocket from 'partysocket'
-import { onUnmounted } from 'vue'
+import { onUnmounted, ref } from 'vue'
+import confetti from 'canvas-confetti'
 
 const ws = new PartySocket({
   host: window.location.host,
   room: 'room1',
   party: 'my-server',
 })
+const msg = ref('Hello, from browser!')
+const serverMsg = ref('')
 
-ws.send('hello from the client!')
+function sendMsg() {
+  ws.send('Hello, from browser!')
+}
 
 const controller = new AbortController()
 ws.addEventListener(
   'message',
   (message) => {
     console.log('message from server:', message.data)
+    serverMsg.value = message.data
+    if (message.data.includes('server!')) {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+      })
+    }
   },
   controller,
 )
@@ -24,7 +37,21 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Hello, browser!</h1>
+  <div class="w-screen h-screen align-middle flex flex-col gap-2 justify-center items-center">
+    <div>
+      <h1 class="text-xl font-medium">Vue 🟢 + Partykit 🎈 + Vite ⚡</h1>
+    </div>
+    <div>
+      <input v-model="msg" class="border py-1 px-2" />
+      <button
+        class="bg-neutral-100 px-2 py-1 border border-neutral-200 hover:bg-neutral-200 active:bg-neutral-300 focus:outline-none focus:ring-1 focus:ring-neutral-300 transition-colors duration-150"
+        @click="sendMsg"
+      >
+        Send Message
+      </button>
+    </div>
+    <div>
+      <p>{{ serverMsg }}</p>
+    </div>
   </div>
 </template>
--- worker/server.ts
+++ worker/server.ts
@@ -1,9 +1,17 @@
 import { Server, type Connection, routePartykitRequest } from 'partyserver'
 
 export class MyServer extends Server<Env> {
-  onMessage(connection: Connection, message: string) {
+  async onMessage(connection: Connection, message: string) {
     console.log('message from client:', message)
-    connection.send('hello from the server!')
+    const response = 'Hello, from the server!'
+    let currentMessage = ''
+
+    // Send growing message with random delays
+    for (const letter of response) {
+      await new Promise((resolve) => setTimeout(resolve, 50 + Math.random() * 150))
+      currentMessage += letter
+      connection.send(currentMessage)
+    }
   }
 }
 
